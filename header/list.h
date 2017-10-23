81509701156e9ba0fc461309d0a50d069a2892ab
falke727 <r201470039hs@kanagawa-u.ac.jp>
2017-10-23 16:44:53 +0900

    add list of pair of unsigned

--- header/list.h
+++ header/list.h
@@ -7,6 +7,7 @@
 #ifndef __TANAKALAB_LIST_H__
 #define __TANAKALAB_LIST_H__
 
+/********** pair of string **********/
 struct STRING_CELL {
   char* key;
   struct STRING_CELL* prev;
@@ -28,5 +29,28 @@ void string_list_delete_sub(string_list*, string_cell*);
 string_list* string_lists_concat(string_list*, string_list*);
 void string_list_clear(string_list*);
 void show_string_list(string_list*);
+
+/********** pair of unsigned **********/
+struct PAIR_UNSIGNED_CELL {
+  pair_unsigned key;
+  struct PAIR_UNSIGNED_CELL* prev;
+  struct PAIR_UNSIGNED_CELL* next;
+};
+typedef struct PAIR_UNSIGNED_CELL pair_unsigned_cell;
+
+struct PAIR_UNSIGNED_LIST {
+  pair_unsigned_cell* head;
+  pair_unsigned_cell* last;
+};
+typedef struct PAIR_UNSIGNED_LIST pair_unsigned_list;
+  
+pair_unsigned_cell* pair_unsigned_list_search(pair_unsigned_list*, pair_unsigned);
+void pair_unsigned_list_insert(pair_unsigned_list*, pair_unsigned);
+void pair_unsigned_list_insert_sub(pair_unsigned_list*, pair_unsigned_cell*);
+void pair_unsigned_list_delete(pair_unsigned_list*, pair_unsigned);
+void pair_unsigned_list_delete_sub(pair_unsigned_list*, pair_unsigned_cell*);
+pair_unsigned_list* pair_unsigned_lists_concat(pair_unsigned_list*, pair_unsigned_list*);
+void pair_unsigned_list_clear(pair_unsigned_list*);
+void show_pair_unsigned_list(pair_unsigned_list*);
 
 #endif
